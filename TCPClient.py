89df43ef4b77854793a920495805223707744327
Arcadev <98187241+AAArcadev@users.noreply.github.com>
2024-10-18 20:32:16 -0400

    Add files via upload

---
+++ TCPClient.py
@@ -0,0 +1,39 @@
+# Import socket module
+from socket import * 
+import sys # In order to terminate the program
+
+serverName = 'localhost'
+# Assign a port number
+serverPort = 120
+
+# Bind the socket to server address and server port
+clientSocket = socket(AF_INET, SOCK_STREAM)
+
+clientSocket.connect((serverName, serverPort))
+
+# try:
+# Recieves client number from server and prints it
+clientNo = clientSocket.recv(1024)
+print(clientNo.decode())
+
+while True:
+        sentence = input('Enter message: ')
+        if (sentence != 'exit'):
+            clientSocket.send(sentence.encode())
+        else:
+            print('closing socket')
+            clientSocket.close()
+            break
+
+        while True:
+            modifiedSentence = clientSocket.recv(1024)
+            print('Server Response:', modifiedSentence.decode())
+            break
+    
+# finally:
+    # print('closing socket')
+    # clientSocket.close()
+
+
+
+#clientSocket.close()
